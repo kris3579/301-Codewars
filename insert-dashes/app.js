53d94b58a7f0f5a00a16140c14be75845041b55e
Kris3579 <kristianesvelt@hotmail.com>
2018-08-16 10:24:01 -0700

    work

--- insert-dashes/app.js
+++ insert-dashes/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let num = '7348579';
-let finalNum;
+let testNum = '7348579';
 function insertDash(num) {
+  let newString = '';
   for (let i = 0; i < num.length; i++) {
+    newString = newString + num[i];
     if (num[i] % 2 !== 0) {
-      if (num[i] % 2 !== 0) {
-        let numbersString = num.slice(0, i) + '-' + num.slice(i, num.length);
-        finalNum = numbersString;
+      if (num[i + 1] % 2 !== 0) {
+        newString = newString + '-';
       };
     };
-    return finalNum;
   };
+  return newString;
 };
